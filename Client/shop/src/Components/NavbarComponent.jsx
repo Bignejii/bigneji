0fe0adfb8fd561c67d105ae61c623b7f0735fed3
bigneji <bigneji@gmail.com>
2025-05-20 05:40:43 -0700

    added some icons

--- Client/shop/src/Components/NavbarComponent.jsx
+++ Client/shop/src/Components/NavbarComponent.jsx
@@ -24,10 +24,17 @@ function NavbarComponent() {
     moveTo(`/products?category=${category}`);
   };
 
+  const handleProfileClick = () => {
+    if (isLoggedIn) {
+      moveTo('/profile');
+    } else {
+      moveTo('/login');
+    }
+  };
+
   return (
     <Navbar bg="transparent" expand="lg" className="position-fixed w-100 navbar-custom">
       <Container className="d-flex justify-content-between align-items-center">
-        {/* Left Section: Links */}
         <Nav className="d-flex gap-4">
           <Nav.Link href="/" className="text-white">Home</Nav.Link>
           <Nav.Link onClick={() => Category('sports')} className="text-white">Sports</Nav.Link>
@@ -37,22 +44,18 @@ function NavbarComponent() {
           <Nav.Link onClick={() => Category('')} className="text-white">All Products</Nav.Link>
         </Nav>
 
-        {/* Right Section: Utility Links */}
         <Nav className="d-flex gap-4 align-items-center">
-          <Nav.Link href="/profile" className="text-white">
-            <i className="bi bi-person"></i>
-          </Nav.Link>
-          <Nav.Link href="/search" className="text-white">
-            <i className="bi bi-search"></i>
-          </Nav.Link>
+          {!isLoggedIn && (
+            <Nav.Link onClick={handleProfileClick} className="text-white">
+              <i className="bi bi-person"></i>
+            </Nav.Link>
+          )}
           {isLoggedIn ? (
             <>
               <Nav.Link onClick={handleSignOut} className="text-white">Sign Out</Nav.Link>
               <Nav.Link onClick={() => moveTo('/manage-products')} className="text-white">Manage Products</Nav.Link>
             </>
-          ) : (
-            <Nav.Link onClick={() => moveTo('/login')} className="text-white">Login</Nav.Link>
-          )}
+          ) : null}
         </Nav>
       </Container>
       <hr className="navbar-divider" />
